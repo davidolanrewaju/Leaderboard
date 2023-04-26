f5c1d25b891a341465ebc78d423a5cc3e84c891b
David <davidolanrewaju538@gmail.com>
2023-04-26 21:27:47 +0100

    Install Webpack and Project dependencies

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const addElem = document.createElement('h1');
+addElem.innerText = 'Hello World';
+document.body.appendChild(addElem);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
