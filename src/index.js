0c20ea766aa03ff6dbf9c597c111dd5ed5a174f9
Dave <davidolanrewaju538@gmail.com>
2023-04-27 11:05:39 +0100

    Update src folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './style.css';
 
 import { displayLeaderboard } from './modules/add-to-list';
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayLeaderboard();
+})
--- src/modules/add-to-list.js
+++ src/modules/add-to-list.js
@@ -1,50 +1,30 @@
 const inputName = document.querySelector('#name');
 const inputScore = document.querySelector('#score');
 const addBtn = document.querySelector('#add-btn');
-const leaderboardSection = document.querySelector('#leaderboard');
+const displayList = document.querySelector('.leaderboard-list');
 // const refreshBtn = document.querySelector('refresh-btn');
 
 const details = [];
 
-// function addToList(content) {
-//   const displayDetails = document.createElement('p');
-//   displayDetails.classList.add('display-details');
-//   displayDetails.innerText += `${content.name}: ${content.score}`;
-//   leaderboardSection.appendChild(displayDetails);
-// }
-
 function displayLeaderboard() {
-  leaderboardSection.innerHTML = ''; // clear leaderboard section
+  displayList.innerText = ''; // clear leaderboard section
   details.forEach((content) => {
     const displayDetails = document.createElement('p');
     displayDetails.classList.add('display-details');
-    displayDetails.innerText += `${content.name}: ${content.score}`;
-    leaderboardSection.appendChild(displayDetails);
+    displayDetails.innerHTML += `
+      <b>${content.name}</b>: ${content.score}
+    `;
+    displayList.appendChild(displayDetails);
   });
 }
 
 addBtn.addEventListener('click', () => {
   const name = inputName.value;
   const score = inputScore.value;
-  const content = { name, score }; // shorthand object notation
+  const content = { name, score };
   details.push(content);
   displayLeaderboard();
 });
 
 // eslint-disable-next-line import/prefer-default-export
 export { displayLeaderboard };
-
-// class GetDetails () {
-//   constructor(inputName, inputScore){
-//     this.inputName = inputName;
-//     this.inputScore = inputScore;
-//   }
-
-//   function name() {
-//     return getName = inputName.value;
-//   }
-
-//   function score() {
-//     return getScore = inputScore.value;
-//   }
-// }
